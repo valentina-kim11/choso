9ed294bd63dc9b13819a3606b24c3fe6394b7872
Valentina Kim <vortegap@unal.edu.co>
2025-06-16 17:56:36 +0700

    Add theme mode seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-    
+
+        $this->call(ThemeModeSettingSeeder::class);
+
     }
 }
